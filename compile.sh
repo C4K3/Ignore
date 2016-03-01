3f379cd1bf39a0d3fd862211432bdfc5e17dc4bf
Sebastian Blunt <sebastian@c4k3.net>
2016-03-01 11:52:51 +0100

    Update to bukkit 1.8.8 Fixes MethodNotFound exception in 1.9

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 RT_JAR=/usr/lib/jvm/java-8-openjdk/jre/lib/rt.jar
 INCLUDES_DIR=../
-BUKKIT_TARGET=bukkit-1.7.9B1938.jar
+BUKKIT_TARGET=bukkit-1.8.8B1.jar
 DST_DIR=../Server/plugins
 
 javac -Xlint:all -bootclasspath "$RT_JAR:$INCLUDES_DIR/$BUKKIT_TARGET" -d ./ src/*
